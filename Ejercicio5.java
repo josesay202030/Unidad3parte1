8e9f1aafc392c12198f24094e0cfbd30671ecccd
José Say <josesay202030974@cunoc.edu.gt>
2021-06-09 11:22:35 -0600

    Código Ejericio5

---
+++ Ejercicio5.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Ejercicio5{
+
+	public static void main (String[] args){
+	Scanner scanner = new Scanner(System.in);
+	
+	int numero;
+	int siguiente;
+
+	System.out.println("Ingrese Número");
+	numero= scanner.nextInt();
+
+	siguiente= numero+1;
+	System.out.println("El número siguiente es: "+siguiente);
+
+	}
+}
