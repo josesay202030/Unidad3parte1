6b410a83a0fde4e99afe0c0470791a3e4ca77bcb
José Say <josesay202030974@cunoc.edu.gt>
2021-06-09 12:12:40 -0600

    Código Ejercicio9

---
+++ Ejercicio9.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Ejercicio9{
+
+        public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+	int A;
+	int B;
+
+	System.out.println("Ingrese Valor A");
+	A= scanner.nextInt();
+
+	System.out.println("Ingrese Valor B");
+	B= scanner.nextInt();
+
+	System.out.println("El Valor intercambiado de A es: "+B);
+	System.out.println("El Valor intercambiado de B es: "+A);
+
+	}
+}
