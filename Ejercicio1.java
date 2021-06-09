2d6effc2499fd22dad0eadbabd1b30a464d15fe4
José Say <josesay202030974@cunoc.edu.gt>
2021-06-09 10:44:36 -0600

    Código Ejercicio1

---
+++ Ejercicio1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Ejercicio1{ 
+
+	public static void main (String[] args){
+	Scanner scanner = new Scanner(System.in);
+		int resultado;
+		int num1;
+		int num2;
+
+		System.out.println("Ingrese el primer Número");
+		num1= scanner.nextInt();
+
+		System.out.println("Ingrese el segundo Número");
+		num2= scanner.nextInt();
+
+		resultado= num1+num2;
+		System.out.println("La suma es : "+resultado);
+
+
+	}
+
+
+
+}
